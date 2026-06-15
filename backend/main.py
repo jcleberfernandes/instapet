8cf0c690dae02754623cb16bac306861236517dc
SRamoras <120826220+SRamoras@users.noreply.github.com>
2026-06-15 19:08:37 +0000

    feat: add CORSMiddleware

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,26 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import create_db
 from routers import auth, users, posts
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("CRIANDO BANCO... CRIANDO BANCO... CRIANDO BANCO... CRIANDO BANCO... CRIANDO BANCO... CRIANDO BANCO... CRIANDO BANCO... CRIANDO BANCO... CRIANDO BANCO...")
+    print("CRIANDO BANCO...")
     create_db()
     yield
 
 
 app = FastAPI(title="InstaPet", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
@@ -20,4 +29,4 @@ app.include_router(posts.router)
 
 @app.get("/")
 def root():
-    return {"message": "InstaPet 🐾"}   
+    return {"message": "InstaPet 🐾"}

fa88413e00f96c1aa171818c0f233a9912c9f48b
SRamoras <120826220+SRamoras@users.noreply.github.com>
2026-06-16 19:34:50 +0000

    refactor: Remove old backend files after reorganization
    - Clean up old file locations from backend/ root
    - All files now located in backend/app/ subdirectory

--- backend/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from models.user import User
-from models.post import Post
-from models.tag import Tag, PostTag
-from models.like import Like
-from models.save import Save
-from models.comment import Comment
-
-__all__ = ["User", "Post", "Tag", "PostTag", "Like", "Save", "Comment"]
--- backend/models/save.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timezone
-from sqlmodel import SQLModel, Field
-
-class Save(SQLModel, table=True):
-    user_id: int = Field(foreign_key="user.id", primary_key=True)
-    post_id: int = Field(foreign_key="post.id", primary_key=True)
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
